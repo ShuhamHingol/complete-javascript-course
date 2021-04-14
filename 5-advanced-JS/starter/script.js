1595dd5fd5c0bc0eb24c9fbc886ff9c4c19d4af6
Shubham Hingol <vinodhingol107@gmail.com>
2021-04-14 19:41:51 +0530

    test

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -392,6 +392,7 @@ c) correct answer (I would use a number for this)
 11. Display the score in the console. Use yet another method for this.
 */
 
+/*
 (function(){
     function Question(question,answers,correct){
         this.question=question;
@@ -467,12 +468,12 @@ c) correct answer (I would use a number for this)
         questions[n].checkAnswer(parseInt(answer),keepScore);
 
         nextQuestion();
-    }    
+        }    
     }
     nextQuestion();
 })();
 
-
+*/
 
 
 
